2018afc6b8755f0767a0525b7b1939b6fcaaea6c
Elsman <ahmedelsman0@gmail.com>
2024-07-14 11:33:27 +0300

    [Feature] create interactor for listing view to get data from api, this way of getting data  need to be enhanced later

--- universities/ModuleA/Interactor/ListingInteractor.swift
+++ universities/ModuleA/Interactor/ListingInteractor.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+protocol ListingInteractorInput {
+    func fetchUniversities()
+}
+
+protocol ListingInteractorOutput: AnyObject {
+    func didFetchUniversities(_ universities: [University])
+    func didFailToFetchUniversities(with error: Error)
+}
+
+class ListingInteractor: ListingInteractorInput {
+    weak var output: ListingInteractorOutput?
+    private let apiUrl = "http://universities.hipolabs.com/search?country=United%20Arab%20Emirates"
+    
+    func fetchUniversities() {
+        if let url = URL(string: apiUrl) {
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    return
+                }
+                
+                if let data = data {
+                    do {
+                        let universities = try JSONDecoder().decode([University].self, from: data)
+                        self.output?.didFetchUniversities(universities)
+                    } catch {
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
