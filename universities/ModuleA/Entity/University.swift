56dab15df168929700b858cc1cf389f2afd174b0
Elsman <ahmedelsman0@gmail.com>
2024-07-14 11:09:06 +0300

    [Feature] create University model

--- universities/ModuleA/Entity/University.swift
+++ universities/ModuleA/Entity/University.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct University: Codable {
+    let name: String
+    let country: String
+    let alphaTwoCode: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name, country
+        case alphaTwoCode = "alpha_two_code"
+    }
+}
